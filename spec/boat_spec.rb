fb6a151671d0d2817f57fa83c87277fd5bb8135b
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-31 20:39:00 -0600

    Feat:complete hours_rented and add_hour methods and tests for class Boat.

--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -20,4 +20,27 @@ RSpec.describe Boat do
       expect(kayak.price_per_hour).to eq(20)
     end
   end 
+
+  describe '#hours_rented' do 
+    it 'will show the amount of hours the boat has been rented' do 
+      kayak = Boat.new(:kayak, 20)
+
+      expect(kayak.hours_rented).to eq(0)
+    end 
+
+    it 'will add hours rented' do
+      kayak = Boat.new(:kayak, 20)
+      kayak.add_hour
+
+      expect(kayak.hours_rented).to eq(1)
+
+      kayak.add_hour
+
+      expect(kayak.hours_rented).to eq(2)
+
+      kayak.add_hour
+
+      expect(kayak.hours_rented).to eq(3)
+    end 
+  end 
 end 
